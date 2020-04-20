3698f606b8198e4bd2d1971a4601fc76c2d7e50d
Raúl IP <raulibpe@gmail.com>
2020-04-20 20:39:13 +0200

    chore(demo)

--- projects/rautils-demo/src/app/app.component.ts
+++ projects/rautils-demo/src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
 	public libraries: {path: string, name: string}[] = [
 		{name: 'NgxTabFocus', path: 'ngx-tab-focus'}
 	];
+
+	constructor(private route: ActivatedRoute, private router: Router) {}
+
+	ngOnInit(): void {
+		this.route.queryParams.subscribe((params) => {
+			if (params.hasOwnProperty('library')) {
+				if (this.libraries.find((library) => library.path === params.library)) {
+					this.router.navigate(['/', params.library]);
+				}
+			}
+		})
+	}
 
 }
