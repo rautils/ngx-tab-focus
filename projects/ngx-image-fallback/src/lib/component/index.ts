15ce74716dfc9d508cb95836fbeff62cc13097eb
Raúl <raulibpe@gmail.com>
2020-04-30 13:51:05 +0200

    chore(main): modified github-actions

--- projects/ngx-image-fallback/src/lib/component/index.ts
+++ projects/ngx-image-fallback/src/lib/component/index.ts
@@ -1,1 +1,2 @@
+export * from './image-attributes.abstract.component';
 export * from './ngx-image-fallback.component';
--- projects/ngx-image-fallback/src/lib/ngx-image-fallback.module.ts
+++ projects/ngx-image-fallback/src/lib/ngx-image-fallback.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgxImageFallbackComponent } from './component/ngx-image-fallback.component';
+import { ImageAttributeAbstractComponent, NgxImageFallbackComponent } from './component';
 
 @NgModule({
-	declarations: [NgxImageFallbackComponent],
+	declarations: [ImageAttributeAbstractComponent, NgxImageFallbackComponent],
 	imports: [CommonModule],
 	exports: [NgxImageFallbackComponent],
 })
