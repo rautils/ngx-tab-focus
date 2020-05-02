b427fe567a92baf039e1c5f75627518da465b218
Raúl <raulibpe@gmail.com>
2020-05-02 23:12:53 +0200

    test(ngx-fake-encapsulation: add basics tests

--- projects/ngx-fake-encapsulation/jest.config.js
+++ projects/ngx-fake-encapsulation/jest.config.js
@@ -3,14 +3,15 @@ var preset = require('jest-preset-angular/jest-preset');
 module.exports = {
 	...preset,
 	preset: 'jest-preset-angular',
-	setupFilesAfterEnv: ['<rootDir>/node_modules/jest-preset-angular/build/setupJest.js'],
+	setupFilesAfterEnv: ['<rootDir>/projects/ngx-fake-encapsulation/setup-jest.ts'],
 	testMatch: ['<rootDir>/projects/ngx-fake-encapsulation/src/**/*.spec.ts'],
 	globals: {
 		...preset.globals,
 		'ts-jest': {
 			...preset.globals['ts-jest'],
 			tsConfig: '<rootDir>/projects/ngx-fake-encapsulation/tsconfig.spec.json',
-			isolatedModules: true,
+			isolatedModules: true
 		},
 	},
+	collectCoverage: true
 };
---
+++ projects/ngx-fake-encapsulation/src/lib/directive/ngx-fake-encapsulation.spec.ts
@@ -0,0 +1,50 @@
+import { NgxFakeEncapsulationModule } from './../ngx-fake-encapsulation.module';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { Component, NgModule } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+@Component({
+	selector: `fake-encapsulation-container`,
+	template: `<div><div id="encapsulated" ngxFakeEncapsulation></div></div>`,
+})
+class NgxFakeEncapsulationInnerTesterComponent {}
+
+@Component({
+	selector: `fake-encapsulation-tester`,
+	template: `<fake-encapsulation-container _nghost-test></fake-encapsulation-container>`,
+})
+class NgxFakeEncapsulationOutterTesterComponent {}
+
+@NgModule({
+	declarations: [NgxFakeEncapsulationInnerTesterComponent, NgxFakeEncapsulationOutterTesterComponent],
+	imports: [NgxFakeEncapsulationModule],
+	exports: [NgxFakeEncapsulationInnerTesterComponent, NgxFakeEncapsulationOutterTesterComponent],
+})
+class NgxFakeEncapsulationTesterModule {}
+
+describe(`NgxFakeEncapsulationDirective`, () => {
+	let fixture: ComponentFixture<NgxFakeEncapsulationOutterTesterComponent>;
+	let encapsulated: HTMLElement;
+
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			imports: [NgxFakeEncapsulationTesterModule],
+		}).compileComponents();
+	}));
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(NgxFakeEncapsulationOutterTesterComponent);
+		fixture.detectChanges();
+
+		encapsulated = fixture.debugElement.query(By.css(`#encapsulated`)).nativeElement;
+	});
+
+	it(`creates`, () => {
+		expect(encapsulated).toBeTruthy();
+	});
+
+	it(`gets the proper encapsulation`, () => {
+		expect(encapsulated.getAttributeNames().includes(`_ngcontent-test`)).toBe(true);
+
+	});
+});
