a4acde9c1fe1b62636ea3fc1d29d09d877363f7c
Raúl <raulibpe@gmail.com>
2020-05-03 16:59:42 +0200

    fix(demo): remove route modification with skipLocationChange property
    Otherwise, it will navigate to demo/library-name, and by doing so any user that tried to go to that url would get a 404 because of github-pages

--- projects/rautils-demo/src/app/pages/main/app.component.ts
+++ projects/rautils-demo/src/app/pages/main/app.component.ts
@@ -2,16 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
-	public libraries: {path: string, name: string}[] = [
-		{name: 'NgxTabFocus', path: 'ngx-tab-focus'},
-		{name: 'NgxImageFallback', path: 'ngx-image-fallback'},
-		{name: 'NgxFakeEncapsulation', path: 'ngx-fake-encapsulation'},
+	public libraries: { path: string; name: string }[] = [
+		{ name: 'NgxTabFocus', path: 'ngx-tab-focus' },
+		{ name: 'NgxImageFallback', path: 'ngx-image-fallback' },
+		{ name: 'NgxFakeEncapsulation', path: 'ngx-fake-encapsulation' },
 	];
 
 	constructor(private route: ActivatedRoute, private router: Router) {}
@@ -20,10 +19,9 @@ export class AppComponent implements OnInit {
 		this.route.queryParams.subscribe((params) => {
 			if (params.hasOwnProperty('library')) {
 				if (this.libraries.find((library) => library.path === params.library)) {
-					this.router.navigate(['/', params.library]);
+					this.router.navigate(['/', params.library], { skipLocationChange: true });
 				}
 			}
-		})
+		});
 	}
-
 }
